5497d22f62f98fdc8ad04b3b75935594a3c21bbf
Mohammad Abdur Rehman Cheema <59124007+Hami0095@users.noreply.github.com>
2022-07-26 22:22:40 +0500

    Create lesson2.cpp

---
+++ lesson2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>	// HEADER FILES
+#include<string>
+
+using namespace std;
+
+
+
+
+
+int main() {
+
+	// OPERAtORS -- OPERANDS -- TYPES OF OPERATORS: 	
+	// + - * /  ==> ARithmetic 
+	// ==, >=, <=, !=, >, <   ==> Relational operators
+	// && (and), || (or), ! (not) ==> logical operators
+	// = Assignment operator
+
+	// Unary operator(operand1);
+
+
+	int a = 9, b= 9;
+	//a = a + b;	// binary operator(operand1, operand2)
+	//cout << a << endl // 9
+	//	<< b;	// 9
+	//
+	// Conditionals :
+	bool c1 = true; 
+	bool c2 = false;
+	bool c3 = true;
+	if (c1 && c2 && c3) {
+		cout << "I am in first conditional" << endl;
+	}
+	
+	 if(!c1 || c2 || !(a!=9)){
+		cout << "I am in second conditional" << endl;
+
+	}
+	else {
+		cout << "I am in else" << endl;
+	}
+
+	
+
+	return 0;
+}
