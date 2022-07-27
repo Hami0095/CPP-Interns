7f0e37ae9163a431d7d7eb9d050396ff0ffc26de
Mohammad Abdur Rehman Cheema <59124007+Hami0095@users.noreply.github.com>
2022-07-27 22:02:47 +0500

    added Else-if case

--- lesson2.cpp
+++ lesson2.cpp
@@ -34,7 +34,9 @@ int main() {
 	 if(!c1 || c2 || !(a!=9)){
 		cout << "I am in second conditional" << endl;
 
-	}
+	}else if(c1||c2 && c3){
+		 cout << "I am in else if conditional" << endl;
+	 }
 	else {
 		cout << "I am in else" << endl;
 	}
